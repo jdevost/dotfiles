90e31973bae7a71282d23778d619881dc9805646
JDevost <jdevost@coveo.com>
2019-12-03 11:12:02 -0500

    add init_dotfiles.sh

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ My own preferences files.
 ## osx_install.sh
 
 Change some OSX defaults to be more practical. It's a merge between [mathiasbynens](https://github.com/mathiasbynens/dotfiles/blob/master/.macos), [pastjean](https://github.com/pastjean/dotfiles/blob/master/installs/osx_install.sh), and my own preferences.
+
+
+## init_dotfiles.sh
+
+When starting a new project, call `sh path/to/init_dotfiles.sh` from your new project's root. It will copy your default dot files in your new project (won't override if some already exists).
---
+++ init_dotfiles.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR=`pwd`
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [ $CURRENT_DIR == $BASE_DIR ]; then
+  echo 'same directory, exiting.'
+  exit 1
+fi
+
+
+if [ ! -d "$CURRENT_DIR/.vscode" ]; then
+  echo 'create .vscode'
+  mkdir "$CURRENT_DIR/.vscode"
+fi
+
+# cp "$BASE_DIR/.vscode/*" "$CURRENT_DIR/.vscode/"
+for file in $BASE_DIR/.vscode/*; do
+  filename="${file##*/}"
+  if [ -f $CURRENT_DIR/.vscode/$filename ]; then
+    echo "skip $filename"
+  else
+    echo "cp $filename"
+    cp "$file" "$CURRENT_DIR/.vscode"
+  fi
+done
+
+
+for file in ".gitignore" ".eslintrc.json" ".jshintrc"; do
+  if [ -f $CURRENT_DIR/$file ]; then
+    echo "skip $file"
+  else
+    echo "cp $file"
+    cp "$BASE_DIR/$file" "$CURRENT_DIR/"
+  fi
+done
+
